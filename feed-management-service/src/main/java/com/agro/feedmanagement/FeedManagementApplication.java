add9fa9f11355c35f39972abaa4bfc5b055f88e7
marcossssantos1 <marcossantosmsms604@gmail.com>
2026-04-13 20:03:18 -0300

    subindo a api de feed-management-service, o aplication.properties foi configurado com o mysql e o rabbitmq

---
+++ feed-management-service/src/main/java/com/agro/feedmanagement/FeedManagementApplication.java
@@ -0,0 +1,12 @@
+package com.agro.feedmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedManagementApplication.class, args);
+    }
+}
