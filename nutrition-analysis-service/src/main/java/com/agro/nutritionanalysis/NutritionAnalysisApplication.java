25effa93195993423900637169c8c655f1920515
marcossssantos1 <marcossantosmsms604@gmail.com>
2026-04-13 23:47:41 -0300

    api nutrition-analysis-service foi adicionada ao projeto e configurado o mongodb e rabbitmq

---
+++ nutrition-analysis-service/src/main/java/com/agro/nutritionanalysis/NutritionAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.agro.nutritionanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutritionAnalysisApplication.class, args);
+    }
+}
